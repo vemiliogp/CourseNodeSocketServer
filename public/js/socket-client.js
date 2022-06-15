14c234389243325c32d50846ed3675e390cd9f19
Victor E Gonzalez P <vemiliogp@gmail.com>
2022-06-15 20:35:26 +0200

    Primer commit

---
+++ public/js/socket-client.js
@@ -0,0 +1,39 @@
+// Referencias del HTML
+const lblOnline = document.querySelector("#lblOnline");
+const lblOffline = document.querySelector("#lblOffline");
+const txtMensaje = document.querySelector("#txtMensaje");
+const btnEnviar = document.querySelector("#btnEnviar");
+
+const socket = io();
+
+socket.on("connect", () => {
+  console.log("Connected");
+
+  lblOffline.style.display = "none";
+  lblOnline.style.display = "";
+});
+
+socket.on("disconnect", () => {
+  console.log("Disconnected");
+
+  lblOnline.style.display = "none";
+  lblOffline.style.display = "";
+});
+
+socket.on("enviar-mensaje", (payload) => {
+  console.log(payload);
+});
+
+btnEnviar.addEventListener("click", () => {
+  const mensaje = txtMensaje.value;
+
+  const payload = {
+    mensaje,
+    id: "123ABC",
+    fecha: new Date().getTime(),
+  };
+
+  socket.emit("enviar-mensaje", payload, (id) => {
+    console.log("From id", id);
+  });
+});
---
+++ sockets/controller.js
@@ -0,0 +1,18 @@
+const socketController = (socket) => {
+  console.log("Client connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected", socket.id);
+  });
+
+  socket.on("enviar-mensaje", (payload, callback) => {
+    const id = 123456;
+    callback(id);
+
+    socket.broadcast.emit("enviar-mensaje", payload);
+  });
+};
+
+module.exports = {
+  socketController,
+};
